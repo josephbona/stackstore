ac3457c8323bbc058e78c18366c99889babb2578
Joseph Bona <joseph.bona07@gmail.com>
2016-10-19 11:41:51 -0400

    allow seed from process.ENV.SYNC

--- seed.js
+++ seed.js
@@ -177,33 +177,33 @@ var seedLineItems = function () {
     return Promise.all(creatingLineItems);
 };
 
-db.sync({ force: true })
-    .then( function(){
-        console.log(chalk.blue('Seed the Users'));
-        return seedUsers();
-    })
-    .then(function(){
-        console.log(chalk.blue('Seed the Products'));
-        return seedProducts();
-    })
-    .then(function(){
-        console.log(chalk.blue('Seed the Categories'));
-        return seedCategories();
-    })
-    .then(function(){
-        console.log(chalk.blue('Seed the Reviews'));
-        return seedReviews();
-    })
-    .then(function(){
-        console.log(chalk.blue('Seed the Line Items'));
-        return seedLineItems();
-    })
+// db.sync({ force: true })
+//     .then( function(){
+//         console.log(chalk.blue('Seed the Users'));
+//         return seedUsers();
+//     })
+//     .then(function(){
+//         console.log(chalk.blue('Seed the Products'));
+//         return seedProducts();
+//     })
+//     .then(function(){
+//         console.log(chalk.blue('Seed the Categories'));
+//         return seedCategories();
+//     })
+//     .then(function(){
+//         console.log(chalk.blue('Seed the Reviews'));
+//         return seedReviews();
+//     })
+//     .then(function(){
+//         console.log(chalk.blue('Seed the Line Items'));
+//         return seedLineItems();
+//     })
 
-    .then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.exit(0);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1);
-    });
+//     .then(function () {
+//         console.log(chalk.green('Seed successful!'));
+//         process.exit(0);
+//     })
+//     .catch(function (err) {
+//         console.error(err);
+//         process.exit(1);
+//     });
