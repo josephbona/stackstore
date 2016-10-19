f25d0fa88fa09140ba6425553c66bea9a294f732
Joseph Bona <joseph.bona07@gmail.com>
2016-10-18 23:22:36 -0400

    link up products, add login, new homepage, remove crap

--- browser/js/common/factories/ProductService.js
+++ browser/js/common/factories/ProductService.js
@@ -1,39 +1,41 @@
 app.factory('ProductService', function($http){
 
-	var _product = {}; 
+	var _product = {};
 
 	return {
 
-		product: _product, 
+		product: _product,
 
 		findAll: function(){
 			return $http.get('/api/products')
 			.then(function(result){
 				return result.data;
+			}).catch(function(err) {
+				console.log(err);
 			});
-		}, 
+		},
 
 		findById: function(product){
 			return $http.get('/api/products/' + product.id)
 			.then(function(result){
-				angular.copy(result.data, _product); 
-				return _product; 
+				angular.copy(result.data, _product);
+				return _product;
 			})
 
-		}, 
+		},
 
 		create: function(product){
 			return $http.post('/api/products', product)
 			.then(function(result){
-				angular.copy(result.data, _product); 
-				return product; 
-			}); 
-		}, 
+				angular.copy(result.data, _product);
+				return product;
+			});
+		},
 
 		destroy: function(product){
 			$http.delete('/api/products/' + product.id)
 			.then(function(){
-				angular.copy({}, _product); 
+				angular.copy({}, _product);
 			})
 		}
 	};
