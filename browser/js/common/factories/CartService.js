9b8ccae80be8e21eafd48b4364b23d32a1cb7468
Jen Noborikawa <jen.noborikawa@gmail.com>
2016-10-19 20:34:42 -0400

    cart factory complete

--- browser/js/common/factories/CartService.js
+++ browser/js/common/factories/CartService.js
@@ -1,38 +1,47 @@
 app.factory('CartService', function($http){
 
-	var _cart = {}; 
+	var _cart = []; 
 
 	return {
 
 		cart: _cart, 
 
-		//no existing route for this
-		findByUserId: function(userId){
-			return $http.get('/api/line_items', userId)
+		findByUserId: function(user){
+			return $http.get('/api/line_items/' + user.id)
 			.then(function(result){
 				angular.copy(result.data, _cart); 
 				return cart; 
 			})
 		}, 
 
-		//doesn't match the route (needs product and quantity)
-		// create: function(productId, quantity){
-		// 	return $http.post('/api/line_items')
-		// 	.then(function(result){
-		// 		angular.copy(result.data, _cart); 
-		// 		return cart; 
-		// 	})
-		// }, 
-
-		destroy: function(){
-
+		create: function(lineItem){
+			return $http.post('/api/line_items/' + lineItem.userId + '/' + lineItem.productId, lineItem)
+			.then(function(result){
+				_cart.push(result.data);  
+				return cart; 
+			})
 		}, 
 
-		update: function(){
+		destroy: function(lineItem){
+			return $http.destroy('/api/line_items/' + lineItem.id)
+			.then(function(){
+				var idx = _cart.indexOf(lineItem); 
+				_cart.splice(idx,1);
+				return _cart; 
+			})
+		}, 
 
+		update: function(lineItem){
+			return $http.put('/api/line_items/' + lineItem.id, lineItem)
+			.then(function(result){
+				var idx = _cart.indexOf(lineItem); 
+				_cart.splice(idx, 1, result.data); 
+				return _cart; 
+			})
 		}
 
 
 	}
 
 }) 
+
